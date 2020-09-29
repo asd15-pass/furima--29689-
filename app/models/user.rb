4b25ab0c70a16347dcbed683bfc1c3804a5703c2
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2020-09-29 15:35:36 +0900

    with_option編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,19 @@ class User < ApplicationRecord
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :email, uniqueness: { case_sensitive: true }
-  validates :password, length:{ minimum: 6 },format: { with: VALID_PASSWORD_REGEX,
-    message: "が半角英数字混合である必要がある"}
-  validates :last_name,              presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ } 
-  validates :last_name_kana,         presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
-  validates :first_name,             presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name_kana,        presence: true, format: {with: /\A[ァ-ヶー－]+\z/ } 
-  validates :birth_date,             presence: true
+         validates :email, uniqueness: { case_sensitive: true }
+         validates :password, length:{ minimum: 6 },format: { with: VALID_PASSWORD_REGEX,
+          message: "が半角英数字混合である必要がある"}
+        with_options presence: true do
+          validates :nickname
+          validates :birth_date
+          with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ }  do
+           validates :last_name           
+           validates :first_name  
+          end         
+          with_options format: {with: /\A[ァ-ヶー－]+\z/ }   do
+           validates :last_name_kana       
+           validates :first_name_kana
+          end       
+        end
 end
