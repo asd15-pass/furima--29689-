147abf6ef90f10734380239b7794adfa61273001
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2020-10-04 21:10:18 +0900

    商品詳細表示機能実装１

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   VALID_PASSWORD_REGEX =/\A[-]?[0-9]+(\.[0-9]+)?\z/
   belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_payer
+  belongs_to_active_hash :consignor
+  belongs_to_active_hash :derivery_time
+
   belongs_to :user    
   has_one :purchase
   has_one_attached :image
