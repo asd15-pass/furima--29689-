01e3a4e6ec6c15b806811e6f896fb8b819a7efe6
asd15-pass <70442362+asd15-pass@users.noreply.github.com>
2020-10-11 20:00:28 +0900

    sold

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   belongs_to_active_hash :consignor
   belongs_to_active_hash :derivery_time
 
-  belongs_to :user    
+  belongs_to :user   
+  has_one :order    
   has_one :purchase
   has_one_attached :image
   with_options presence: true do
